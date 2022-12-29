befc29c53c7d9097c2277286b1e8c72841883bc0
rafael <rafael@schiafone.de>
2022-12-29 19:58:07 +0100

    basic setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,52 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use utils::{error::AuthError, App, AuthPlugin, Plugin, UserPlugin};
+
+pub struct Users {}
+
+impl Users {
+    pub fn new() -> Self {
+        Users {}
+    }
+}
+
+impl Plugin for Users {
+    fn init(&self, app: &App) -> Result<(), ()> {
+        let res: Result<(), ()> = match app.register_user_plugin(self) {
+            Ok(_) => Ok(()),
+            Err(_) => return Err(()),
+        };
+
+        let res = match app.register_auth_plugin(self) {
+            Ok(_) => Ok(()),
+            Err(_) => Err(()),
+        };
+
+        res
+    }
+    fn shutdown(&self, _app: &App) -> Result<(), ()> {
+        todo!()
+    }
+
+    fn clone_boxed(&self) -> Box<dyn Plugin> {
+        Box::new(Users {})
+    }
+}
+
+impl UserPlugin for Users {
+    fn render_input(&self) -> String {
+        todo!()
+    }
+
+    fn render_edit(&self, _user_id: &str) -> String {
+        todo!()
+    }
+
+    fn render_display(&self, _user_id: &str) -> String {
+        todo!()
+    }
+}
+
+impl AuthPlugin for Users {
+    fn authenticate(&self, _name: &str, _password: &str) -> Result<(), AuthError> {
+        todo!()
     }
 }
